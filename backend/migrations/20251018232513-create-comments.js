cbcceef8ccf6721eacda275464f1ae562e9f237e
Filip Kowalewski <filip.kowalewski598@gmail.com>
2025-10-18 12:29:25 +0200

    add create-comments migration

---
+++ backend/migrations/20251018232513-create-comments.js
@@ -0,0 +1,39 @@
+/** @type {import('sequelize-cli').Migration} */
+export default {
+  up (queryInterface, Sequelize) {
+    return queryInterface.createTable("comments", {
+      id: {
+        type: Sequelize.DataTypes.INTEGER.UNSIGNED,
+        autoIncrement: true,
+        primaryKey: true,
+        allowNull: false,
+      },
+      author: {
+        type: Sequelize.DataTypes.STRING(30),
+        allowNull: false,
+      },
+      message: {
+        type: Sequelize.DataTypes.STRING(300),
+        allowNull: false,
+      },
+      createdAt: {
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+        allowNull: false,
+      },
+      updatedAt: {
+        type: Sequelize.DataTypes.DATE,
+        defaultValue: Sequelize.fn("NOW"),
+        allowNull: false,
+      },
+      deletedAt: {
+        type: Sequelize.DataTypes.DATE,
+        allowNull: true,
+      },
+    });
+  },
+
+  down (queryInterface, Sequelize) {
+    return queryInterface.dropTable("comments");
+  },
+};
