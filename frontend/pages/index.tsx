6c32889d221831e0b283f532ef1765b96458644e
Filip Kowalewski <filip.kowalewski598@gmail.com>
2025-10-21 17:32:11 +0200

    update page component

--- frontend/pages/index.tsx
+++ frontend/pages/index.tsx
@@ -1,4 +1,23 @@
 import Head from "next/head";
+import { wrapper } from "@/store";
+import { api } from "@/services";
+import {
+  EnhancedCommentsTable,
+  COMMENTS_LIMIT_FILTER,
+} from "@/components/enhanced-comments-table";
+import { CreateCommentDialog } from "@/components/comments-dialogs";
+
+// Investigate possibility of store leaking to other requests (as mentiened here https://redux-toolkit.js.org/rtk-query/usage/server-side-rendering)
+export const getServerSideProps = wrapper.getServerSideProps(
+  (store) => async () => {
+    store.dispatch(
+      api.endpoints.getComments.initiate({ limit: COMMENTS_LIMIT_FILTER })
+    );
+    await Promise.all(store.dispatch(api.util.getRunningQueriesThunk()));
+
+    return { props: {} };
+  }
+);
 
 export default function Home() {
   return (
@@ -13,6 +32,8 @@ export default function Home() {
       <main className="min-h-screen flex items-center justify-center p-5">
         <div className="flex flex-col gap-10 w-full max-w-5xl">
           <h1 className="text-4xl text-center">Comments</h1>
+          <EnhancedCommentsTable />
+          <CreateCommentDialog />
         </div>
       </main>
     </>
