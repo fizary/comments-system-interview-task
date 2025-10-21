d78930c1997acde071bc7c081d129dfcecdea67b
Filip Kowalewski <filip.kowalewski598@gmail.com>
2025-10-21 17:40:38 +0200

    add cors middleware

--- backend/app.js
+++ backend/app.js
@@ -14,6 +14,19 @@ const app = express();
 // Register body parser middleware
 app.use(bodyParser.json());
 
+// Handle CORS
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  next();
+});
+
 // Register app router middleware
 app.use(router);
 
