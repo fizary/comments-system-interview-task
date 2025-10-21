1d8377b21452a898c5a680afa82c8331253e1d5e
Filip Kowalewski <filip.kowalewski598@gmail.com>
2025-10-21 17:50:09 +0200

    add more records to comments seed

--- backend/seeders/20251020041809-comments.js
+++ backend/seeders/20251020041809-comments.js
@@ -1,31 +1,51 @@
 /** @type {import('sequelize-cli').Migration} */
 export default {
-  up (queryInterface, Sequelize) {
+  up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert('comments', [
       {
-        author: "Alice",
-        message: "This article was super helpful, thanks!",
+        "author": "Charlie",
+        "message": "Great write-up! I learned a lot about patterns in JavaScript.",
       },
       {
-        author: "Bob",
-        message: "I think there's a small mistake in the second paragraph.",
+        "author": "Matthew",
+        "message": "I wish more tutorials explained TypeScript generics this clearly.",
       },
       {
-        author: "Charlie",
-        message: "Can you explain more about the database part?",
+        "author": "Ethan",
+        "message": "Really enjoyed the section on performance optimization with React hooks!",
       },
       {
-        author: "Diana",
-        message: "Great write-up! I learned a lot.",
+        "author": "Jacob",
+        "message": "This helped me understand why immutability is so important in Redux.",
       },
       {
-        author: "Ethan",
-        message: "I tried this approach and it worked perfectly!",
+        "author": "Olivia",
+        "message": "Excellent article on Node.js streams - it finally clicked for me!",
       },
+      {
+        "author": "Thomas",
+        "message": "Nice overview of how to structure scalable Express apps. Very practical.",
+      },
+      {
+        "author": "Bob",
+        "message": "Appreciated the examples comparing promises and observables.",
+      },
+      {
+        "author": "Chris",
+        "message": "This article convinced me to finally start using ESLint and Prettier in all my projects.",
+      },
+      {
+        "author": "Alice",
+        "message": "Your explanation of closures was so clear. I will be sharing this with my team.",
+      },
+      {
+        "author": "Peter",
+        "message": "Loved the deep dive into WebSockets. The diagrams made it super easy to follow!",
+      }
     ], {});
   },
 
-  down (queryInterface, Sequelize) {
+  down(queryInterface, Sequelize) {
     return queryInterface.bulkDelete('comments', null, {});
   },
 };
